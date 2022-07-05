25aa888df433b5181de39ab6218e7e0bc46ab9cf
Esad Şen <90692939+Blankbee@users.noreply.github.com>
2022-07-05 15:48:03 +0300

    Add files via upload

---
+++ project_library/app.py
@@ -0,0 +1,12 @@
+from assoc_files.config import app
+
+
+if __name__ == '__main__':
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True,use_reloader=True)
+
+
+
+
+
